e89e03df10454cc7f5fda3b8dd537d5fceb195ab
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2015-11-17 19:27:57 -0300

    Add references between models.

--- src/e-vader/app/models/contact.rb
+++ src/e-vader/app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ActiveRecord::Base
+	has_many :client_contacts
+	has_many :clients, through: :client_contacts
+	
 	validates :contact_type, presence: true
 	validates_uniqueness_of :contact_type
 end
--- src/e-vader/db/schema.rb
+++ src/e-vader/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116014452) do
+ActiveRecord::Schema.define(version: 20151117214450) do
+
+  create_table "client_contacts", force: :cascade do |t|
+    t.integer "client_id"
+    t.integer "contact_id"
+    t.string  "value"
+  end
 
   create_table "clients", force: :cascade do |t|
     t.string   "firstname"
