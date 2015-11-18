0a1bd041d3e6e4f68845addc2d4a8527585a8323
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2015-11-17 21:05:26 -0300

    Added invoice relationship

--- src/e-vader/app/models/client.rb
+++ src/e-vader/app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ActiveRecord::Base
 	has_many :client_contacts
 	has_many :contacts, through: :client_contacts
+	has_many :invoices
 
 	validates :firstname, :lastname, :document_number, :genre, :birthdate, presence: true
 	validates :identification_code_type, :identification_code_number, presence: true
