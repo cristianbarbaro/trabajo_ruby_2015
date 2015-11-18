667334762ef99453f9d12f3e65203b2f25cb3f4b
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2015-11-17 22:22:54 -0300

    Added resource Invoice in rputes

--- src/e-vader/config/routes.rb
+++ src/e-vader/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :invoices
   resources :clients
   resources :contacts
 
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'clients#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
