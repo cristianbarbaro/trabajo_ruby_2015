81578a6f4584a886dc42ec741291bd975f450751
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2015-11-17 22:02:59 -0300

    Validates identification types.

--- src/e-vader/app/models/client.rb
+++ src/e-vader/app/models/client.rb
@@ -6,4 +6,5 @@ class Client < ActiveRecord::Base
 	validates :firstname, :lastname, :document_number, :genre, :birthdate, presence: true
 	validates :identification_code_type, :identification_code_number, presence: true
 	validates :document_number, numericality: { only_integer: true }
+	validates :identification_code_type, inclusion: { in: %w(CUIT CUIL) }
 end
