b8b5d87bcb964889cc23decc59d3a413cdd37dc3
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2015-11-24 00:13:59 -0300

    added rails-i18n gem

--- src/e-vader/config/application.rb
+++ src/e-vader/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module EVader
   class Application < Rails::Application
+    config.web_console.whitelisted_ips = '10.0.2.2'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -18,10 +19,9 @@ module EVader
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.web_console.whitelisted_ips = '10.0.2.2'
   end
 end
--- src/e-vader/config/environments/development.rb
+++ src/e-vader/config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Tells which locale is loaded in application
+  # config.i18n.available_locales = :es
+
 end
