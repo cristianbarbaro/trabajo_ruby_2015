0e8447135448a1bc6fee1c245630c6b09e267fac
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2015-11-26 18:15:23 -0300

    Added index in foreign keys.

--- src/e-vader/db/migrate/20151117214450_create_client_contacts.rb
+++ src/e-vader/db/migrate/20151117214450_create_client_contacts.rb
@@ -1,13 +1,12 @@
 class CreateClientContacts < ActiveRecord::Migration
   def change
     create_table :client_contacts do |t|
-      t.integer :client_id, null: false
-      t.integer :contact_id, null: false
+      t.belongs_to :client, index: true
+      t.belongs_to :contact, index: true
       t.string :value, null: false
 
-      add_foreign_key :client_contacts, :clients
-      add_foreign_key :client_contacts, :contacts
-
+      #add_foreign_key :client_contacts, :clients
+      #add_foreign_key :client_contacts, :contacts
     end
   end
 end
