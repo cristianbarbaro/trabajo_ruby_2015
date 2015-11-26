d59f7bdb98e88c69d5a973cfdbcac08085cd2441
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2015-11-26 12:48:06 -0300

    Added rlationship has many through between client and person.

--- src/e-vader/app/models/client.rb
+++ src/e-vader/app/models/client.rb
@@ -2,6 +2,7 @@ class Client < ActiveRecord::Base
 	has_many :client_contacts
 	has_many :contacts, through: :client_contacts
 	has_many :invoices
+	has_many :people, through: :invoices
 
 	validates :firstname, :lastname, :document_number, :genre, :birthdate, presence: true
 	validates :identification_code_type, :identification_code_number, presence: true
--- src/e-vader/app/models/person.rb
+++ src/e-vader/app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
 	has_many :invoices
+	has_many :clients, through: :invoices
 
 	validates :name, :identification_code_type, :identification_code_number, presence: true
 end
