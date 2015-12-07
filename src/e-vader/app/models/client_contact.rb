5e5e66ad760d2759bce61825d5c4a676838027f7
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2015-12-07 10:03:53 -0300

    Validates parent in nested asttributes when creates models.

--- src/e-vader/app/models/client_contact.rb
+++ src/e-vader/app/models/client_contact.rb
@@ -1,6 +1,7 @@
 class ClientContact < ActiveRecord::Base
-	belongs_to :client
+	belongs_to :client, inverse_of: :client_contacts
 	belongs_to :contact
 
-	validates :value, :client_id, :contact_id, presence: true
+	validates_presence_of :client
+	validates :value, :contact_id, presence: true
 end
